6b0cc1e26f605cca86acc7bc137933735bea870d
judinilson monchacha <judinilson0000@gmail.com>
2022-05-27 19:40:00 +0400

    implementing queries

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,37 @@
-import pins from "../assets/data/pins";
+import { useNhostClient } from "@nhost/react";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+//import pins from "../assets/data/pins";
 
 import MasonryList from "../components/MasonryList";
 
 export default function HomeScreen() {
+  const nhost = useNhostClient();
+  const [pins, setPins] = useState([]);
+
+  const fetchPins = async () => {
+    const response = await nhost.graphql.request(`
+    query MyQuery {
+      Pins {
+        created_at
+        id
+        image
+        title
+        user_id
+      }
+    }
+    `);
+    if (response.error) {
+      Alert.alert("error fetching pins");
+    } else {
+      setPins(response.data.Pins);
+    }
+  };
+
+  //fetchPins every time it changes
+  useEffect(() => {
+    fetchPins();
+  }, []);
+
   return <MasonryList pins={pins} />;
 }
--- screens/PinScreen.tsx
+++ screens/PinScreen.tsx
@@ -2,25 +2,67 @@ import { Ionicons } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React, { Component, useEffect, useState } from "react";
-import { Text, View, StyleSheet, Image, Pressable } from "react-native";
+import { Text, View, StyleSheet, Image, Pressable, Alert } from "react-native";
 import {
   useSafeAreaInsets,
   SafeAreaView,
 } from "react-native-safe-area-context";
+import { useNhostClient } from "@nhost/react";
 import pins from "../assets/data/pins";
 
+const GET_PIN_QUERY = `
+   query MyQuery($id:uuid!) {
+      Pins_by_pk(id: $id) {
+          image
+          id
+          title
+          user_id
+          user {
+            displayName
+            id
+            avatarUrl
+          }
+        created_at
+     }
+    }
+
+`;
+
 const PinScreen = () => {
+  const nhost = useNhostClient();
+  const [pin, setPin] = useState<any>(null);
   const navigation = useNavigation();
   const route = useRoute();
-  //const pin = pins[1];
+
   const [ratio, setRation] = useState(1);
 
   const pinId = route.params?.id;
 
-  const pin = pins.find((p) => p.id == pinId);
-
   //get the size of our notch top screen
   const insets = useSafeAreaInsets();
+
+  const fetchPin = async (pinId) => {
+    const response = await nhost.graphql.request(GET_PIN_QUERY, { id: pinId });
+
+    if (response.error) {
+      Alert.alert("error fetching pins");
+    } else {
+      setPin(response.data.Pins_by_pk);
+    }
+  };
+
+  //fetchPins every time it changes
+  useEffect(() => {
+    fetchPin(pinId);
+  }, [pinId]);
+
+  useEffect(() => {
+    if (pin) {
+      Image.getSize(pin.image, (width, height) => {
+        setRation(width / height);
+      });
+    }
+  }, [pin]);
 
   if (!pin) {
     return <Text>Pin not found</Text>;
@@ -30,13 +72,6 @@ const PinScreen = () => {
     navigation.goBack();
   };
 
-  useEffect(() => {
-    if (pin) {
-      Image.getSize(pin.image, (width, height) => {
-        setRation(width / height);
-      });
-    }
-  }, [pin]);
   return (
     <SafeAreaView style={{ backgroundColor: "black" }}>
       <StatusBar style="light" />
